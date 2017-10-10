045102c71a4c0942811ff2bdfbacd240f5e6d5d2
emil torlen <emil torlen>
2017-10-10 11:58:05 +0200

    added pm2

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Demo for node
 
 1) Download Repo
 2) npm install
+3) npm install gulp -g
 3) gulp
 4) npm start
---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "API",
+      script    : "lib\\app.js",
+      env: {
+        PORT: 3000
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    },
+
+    // Second application
+    {
+      name      : "LoadJs",
+      script    : "lib\\load.js"
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/production",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
+    },
+    dev : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/development",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env dev",
+      env  : {
+        NODE_ENV: "dev"
+      }
+    }
+  }
+}
